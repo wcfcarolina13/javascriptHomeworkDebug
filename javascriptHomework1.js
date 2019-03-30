bee5b9be4c4171839eb12725aa0456a2b3487a8f
Bradley Stone <stoneba1992@gmail.com>
2019-03-29 22:48:24 -0400

    initial commit

---
+++ javascriptHomework1.js
@@ -0,0 +1,53 @@
+// Rock paper scissors
+
+// Computer's choice method
+let player_count = 0;
+let enemy_count = 0;
+
+function game(player_count, enemy_count) {
+	let enemyArray = ["rock", "paper", "scissors"];
+	let game_counter = 0;
+	do {
+		let choice = prompt("Rock, paper, or scissors?");
+		let enemy = enemyArray[Math.floor(Math.random() * enemyArray.length)];
+		console.log(`${enemy}`);
+		// let enemy = document.getElementById('result');
+		game_counter += 1;
+		if (choice == "rock") {
+			if (enemy == "rock") {
+			  console.log("tie");
+			} else if (enemy == "paper") {
+			  console.log("I win");
+			  enemy_count += 1;
+			} else if (enemy == "scissors") {
+			  console.log("I lose");
+			  player_count += 1;
+			}
+		} else if (choice == "paper") {
+			if (enemy == "rock") {
+		  		console.log("I lose");
+		  		player_count += 1;
+		    } else if (enemy == "paper") {
+		  		console.log("Tie");
+			} else if (enemy == "scissors") {
+				console.log("I win");
+				enemy_count += 1;
+		  	}
+		} else if (choice == "scissors") {
+			if (enemy == "rock") {      
+				console.log("I win");
+		  		enemy_count += 1;
+		  	} else if (enemy == "paper") {
+		  		console.log("I lose");
+		  		player_count += 1;
+		  	} else if (enemy == "scissors" ) {
+		 		console.log("Tie");
+		 	}
+		}
+	} while (player_count != 5 || enemy_count != 5);
+	console.log(`Player Score: ${player_count}`);
+	console.log(`Computer Score: ${enemy_count}`);
+	console.log(`Games Played: ${game_counter}`);
+}
+
+// game(enemy_count, player_count, enemyArray);
